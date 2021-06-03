25891c589f0211ed6e6f69c72973ab63644e403d
mksahota10 <mksahota10@gmail.com>
2021-06-02 23:42:20 -0700

    updated broken code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,23 +7,29 @@ const Projects = () => {
     return (
 
         <div>
-           <div class="title">
+           <div class="projecttitle">
  <h1> Projects</h1>
 
  
 </div>
+
       <ul class="collection">
-                <a class="first" href="https://mksahota10.github.io/Project-1---Trivia/"  target="_blank"title="photo" id="photo">FINANCE TRIVIA</a>
-                <br></br>
-                <br></br>
+                <a class="first" href="https://mksahota10.github.io/Project-1---Trivia/"  target="_blank"title="photo" id="photo"></a>
+                <div id="projectname">FINANCE TRIVIA</div>
                 <a href="https://github.com/mksahota10/Project-1---Trivia"target="_blank" id="code"> CODE</a>
-                <a class="second" href="https://drinkitup.herokuapp.com/"  target="_blank"title="photo" id="photo">DRINK UP</a>
                 <br></br>
-                <a href="https://github.com/mksahota10/project2-DrinkUp"target="_blank" id="code1"> CODE</a>
-                <a class="third" href="https://fomo-nomo-frontend.surge.sh/" target="_blank" title="photo" id="photo">FOMO-NOMO</a>
-                <a href="https://github.com/orgs/SFX818/teams/brogrammers/repositories"target="_blank" id="code2"> CODE</a>
-                <a class="fourth" href="https://mksahota10.github.io/Store/"  target="_blank"title="photo" id="photo">WICKS N' MORE</a>
-                <a href="https://github.com/mksahota10/Store/tree/main/ecommerce"target="_blank" id="code2"> CODE</a>
+                <a class="second" href="https://drinkitup.herokuapp.com/"  target="_blank"title="photo" id="photo"></a>
+                <br></br>
+                <div id="projectname">  DRINK UP<br></br> </div>
+                <br></br>
+                <a href="https://github.com/mksahota10/project2-DrinkUp"target="_blank" id="code"> CODE</a>
+                <br></br>
+                <a class="third" href="https://fomo-nomo-frontend.surge.sh/" target="_blank" title="photo" id="photo"></a>
+                <div id="projectname">FOMO-NOMO</div>
+                <a href="https://github.com/orgs/SFX818/teams/brogrammers/repositories"target="_blank" id="code"> CODE</a>
+                <a class="fourth" href="https://mksahota10.github.io/Store/"  target="_blank"title="photo" id="photo"></a>
+                <div id="projectname">WICKS N' MORE</div>
+                <a href="https://github.com/mksahota10/Store/tree/main/ecommerce"target="_blank" id="code"> CODE</a>
                 
               </ul>
             <br></br>
